1c78719b89d661536e036d86ef338eea87b91142
Emmapr123 <emmapriester@hotmail.com>
2021-05-06 17:41:30 +0100

    Refactored the account class to calculate the date in a separate function

--- src/Account.js
+++ src/Account.js
@@ -3,9 +3,12 @@ class Account {
     this.transactions = []
   }
   deposit(amount) {
-    this.transactions.push(new Transaction(amount, 0, this.date = new Date().toLocaleDateString()))
+    this.transactions.push(new Transaction(amount, 0, this.createDate()))
   }
   withdraw(amount) {
-    this.transactions.push(new Transaction(0, amount, this.date = new Date().toLocaleDateString()))
+    this.transactions.push(new Transaction(0, amount, this.createDate()))
+  }
+  createDate() {
+    return new Date().toLocaleDateString()
   }
 }
